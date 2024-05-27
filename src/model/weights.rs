55be0140aa9c22876612152200f5f591c5ed0090
Alex Smaliy <alex.smaliy@gmail.com>
2024-05-26 23:13:57 -0700

    make weight/bias containers lazy

--- src/model/weights.rs
+++ src/model/weights.rs
@@ -4,46 +4,55 @@ use rand::distributions::{Distribution, Uniform};
 use crate::linalg::{Matrix, Vector};
 
 pub enum Biases<const DIM: usize> {
-    Zeros(Vector<DIM>),
-    UniformlyRandom(Vector<DIM>),
+    Zeros,
+    UniformlyRandom { lo: f32, hi: f32 },
 }
 
 impl<const DIM: usize> Biases<DIM> {
     pub fn zeros() -> Self {
-        Self::Zeros(Vector::zero())
+        Self::Zeros
     }
 
     pub fn uniformly_random(lo: f32, hi: f32) -> Self {
+        Self::UniformlyRandom { lo, hi }
+    }
+
+    fn lazy_uniform_random(&self, lo: f32, hi: f32) -> Vector<DIM> {
         let rng = thread_rng();
         let mut uniform = Uniform::<f32>::new(lo, hi).sample_iter(rng);
         let mut arr: [f32; DIM] = [0f32; DIM];
         for i in 0..DIM {
             arr[i] = uniform.next().unwrap();
         }
-        Self::UniformlyRandom(Vector::from_arr(arr))
+        Vector::from_arr(arr)
     }
 }
 
+// No From impl, this is a one-way conversion.
 impl<const DIM: usize> Into<Vector<DIM>> for Biases<DIM> {
     fn into(self) -> Vector<DIM> {
         match self {
-            Self::Zeros(v) => v,
-            Self::UniformlyRandom(v) => v,
+            Self::Zeros => Vector::zero(),
+            s @ Self::UniformlyRandom { lo, hi } => s.lazy_uniform_random(lo, hi),
         }
     }
 }
 
 pub enum Weights<const IN: usize, const OUT: usize> where [(); OUT*IN]: Sized {
-    Zeros(Matrix<OUT, IN>),
-    UniformlyRandom(Matrix<OUT, IN>),
+    Zeros,
+    UniformlyRandom { lo: f32, hi: f32 },
 }
 
 impl<const IN: usize, const OUT: usize> Weights<IN, OUT> where [(); OUT*IN]: Sized {
     pub fn zeros() -> Self {
-        Self::Zeros(Matrix::zero())
+        Self::Zeros
     }
 
     pub fn uniformly_random(lo: f32, hi: f32) -> Self {
+        Self::UniformlyRandom { lo, hi }
+    }
+
+    fn lazy_uniform_random(&self, lo: f32, hi: f32) -> Matrix<OUT, IN> {
         let rng = thread_rng();
         let mut uniform = Uniform::<f32>::new(lo, hi).sample_iter(rng);
         let mut arr: [f32; OUT*IN] = [0f32; OUT*IN];
@@ -51,7 +60,7 @@ impl<const IN: usize, const OUT: usize> Weights<IN, OUT> where [(); OUT*IN]: Siz
             arr[i] = uniform.next().unwrap();
         }
         // uniform.collect::<Vec<_>>().try_into().unwrap(); // TODO: this panics.
-        Self::UniformlyRandom(Matrix::from_arr(arr))
+        Matrix::from_arr(arr)
     }
 }
 
@@ -59,8 +68,8 @@ impl<const IN: usize, const OUT: usize> Weights<IN, OUT> where [(); OUT*IN]: Siz
 impl<const IN: usize, const OUT: usize> Into<Matrix<OUT, IN>> for Weights<IN, OUT> where [(); OUT*IN]: Sized {
     fn into(self) -> Matrix<OUT, IN> {
         match self {
-            Self::Zeros(m) => m,
-            Self::UniformlyRandom(m) => m,
+            Self::Zeros => Matrix::zero(),
+            s @ Self::UniformlyRandom { lo, hi } => s.lazy_uniform_random(lo, hi),
         }
     }
 }
